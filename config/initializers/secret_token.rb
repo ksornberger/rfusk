a7c413b5adfe452041b3b8bf455ef4b94463740d
Kevin Sornberger <ksornberger@gmail.com>
2011-02-02 16:48:07 -0500

    Adding initial Fresh Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rfusk::Application.config.secret_token = 'bc1e79be026bfd9a99f06e92fe80aba79349ce4ff3b9b3aa0831ee60a8b3705225fb4f71cbebf2273483b3eaceabd2ad0a36a6e2433554acdde9e5e07819fee8'
