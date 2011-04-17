73d205804ce22ace8fd257b2845ab7a502df603f
Kevin Sornberger <ksornberger@gmail.com>
2011-04-17 01:21:04 -0400

    Adding initial fusker url form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rfusk::Application.routes.draw do
+  resources :fusks
+
+  get "home/index"
+  
+  match 'fusk' => 'index#fusk'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Rfusk::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
